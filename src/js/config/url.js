c2c1d29abe8c715b9b6cbdba42145b7910659c42
HeroBuxx <me@herobuxx.me>
2024-11-30 00:49:37 +0700

    login: auth: Remove extra slash
    Signed-off-by: HeroBuxx <me@herobuxx.me>

--- src/js/config/url.js
+++ src/js/config/url.js
@@ -1,2 +1,2 @@
 export let authURL = "https://canna.hlcyn.co/api/auth";
-export let reqpassURL = "https://canna.hlcyn.co/api/passkey/";
+export let reqpassURL = "https://canna.hlcyn.co/api/passkey";
